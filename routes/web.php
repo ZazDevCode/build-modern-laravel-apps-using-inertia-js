2e083525bc7d6a5d5f47f93dced463a77d96ad3d
Sascha Wernars <s@scha.nl>
2022-08-19 16:42:54 +0200

    Finished Episode 4: Inertia Links

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Home' , [
-        'name' => 'Joe Doe',
-        'frameworks' => [
-            'Laravel', 'Vue', 'Inertia'
-        ]
-    ]);
+    return Inertia::render('Home');
+});
+
+Route::get('/users', function () {
+    return Inertia::render('Users');
+});
+
+Route::get('/settings', function () {
+    return Inertia::render('Settings');
 });
