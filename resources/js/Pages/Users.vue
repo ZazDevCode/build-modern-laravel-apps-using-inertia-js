d387007e99f132fe7d84952ca2da0960b8c9f33a
Sascha Wernars <s@scha.nl>
2022-08-23 21:23:03 +0200

    Finished Epi 16: An Important SPA Security Concern

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,3 +1,18 @@
 <template>
+<Head title="Users" />
         <h1 class="text-3xl">Users</h1>
+
+<ul>
+        <li 
+                v-for="user in users"
+                :key="user.id"
+                v-text="user.name"
+        />
+</ul>
+
+
 </template>
+
+<script setup>
+defineProps({  users: Array });
+</script>
--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,4 +1,12 @@
 <template>
+    <Head>
+            <title>My App</title>
+            <meta 
+                type="description" 
+                content="Information about my app" 
+                head-key="description"
+            >
+    </Head>
     <section class="p-6 bg-gray-200">
         <header class="flex justify-between">
             <div class="flex items-center">
@@ -22,10 +30,11 @@
 </template>
 
 <script>
-import Nav from './Nav'
+import Nav from './Nav';
+import { Head } from "@inertiajs/inertia-vue3";
 
 export default {
-    components: { Nav },
+    components: { Nav, Head },
 
     computed: {
         username() {
